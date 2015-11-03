a63ebe0da37d3768cde30b27fe37a29d1fa6e741
Merklen <merklen.maxime0811@gmail.com>
2015-11-03 15:09:02 +0100

    ensemble des activités
    ajout de toutes les activités de l'app:

---
+++ Eze2chooz/app/src/main/java/toucoumer/eze2chooz/choix_des_voeux.java
@@ -0,0 +1,13 @@
+package toucoumer.eze2chooz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class choix_des_voeux extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choix_des_voeux);
+    }
+}
