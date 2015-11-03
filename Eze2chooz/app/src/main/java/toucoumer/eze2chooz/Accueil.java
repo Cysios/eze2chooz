d05d1102b2c487b31ff79bee7195765a6a19f03d
Hopetech <elietournier@hotmail.fr>
2015-11-03 16:55:30 +0100

    Ajou lien vue 1 vue 2

--- Eze2chooz/app/src/main/java/toucoumer/eze2chooz/Accueil.java
+++ Eze2chooz/app/src/main/java/toucoumer/eze2chooz/Accueil.java
@@ -1,13 +1,25 @@
 package toucoumer.eze2chooz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Accueil extends AppCompatActivity {
+public class git Accueil extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+
+        final Button ButtonGoToNext = (Button) findViewById(R.id.button_1to2);
+        ButtonGoToNext.setOnClickListener(new onClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, choix_des_voeux.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
