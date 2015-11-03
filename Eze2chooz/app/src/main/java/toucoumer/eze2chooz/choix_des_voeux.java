63eb62f694a239ece134f1980fb13fe099c3398d
Merklen <merklen.maxime0811@gmail.com>
2015-11-03 17:40:38 +0100

    ajout bouton 2-3 java

--- Eze2chooz/app/src/main/java/toucoumer/eze2chooz/choix_des_voeux.java
+++ Eze2chooz/app/src/main/java/toucoumer/eze2chooz/choix_des_voeux.java
@@ -1,7 +1,10 @@
 package toucoumer.eze2chooz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class choix_des_voeux extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class choix_des_voeux extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choix_des_voeux);
+
+        final Button ButtonGoToNext = (Button) findViewById(R.id.button_2to3);
+        ButtonGoToNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(choix_des_voeux.this, secouer.class);
+                startActivity(intent);
+            }
+        });
     }
 }
