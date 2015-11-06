784b5bff99ad161cf0bc49fe3945019c266dcc88
Hopetech <elietournier@hotmail.fr>
2015-11-07 00:53:53 +0100

    Param from choices to shake
    *Add strings value
    *Add parameters to intent
    *Edit GUI

--- Eze2chooz/app/src/main/java/toucoumer/eze2chooz/choices.java
+++ Eze2chooz/app/src/main/java/toucoumer/eze2chooz/choices.java
@@ -4,20 +4,49 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class choices extends AppCompatActivity {
+
+    final String EXTRA_CHOICE1 = "user_choice1";
+    final String EXTRA_CHOICE2 = "user_choice2";
+    final String EXTRA_CHOICE3 = "user_choice3";
+    final String EXTRA_CHOICE4 = "user_choice4";
+    final String EXTRA_CHOICE5 = "user_choice5";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choices);
 
+        final EditText choice1 = (EditText) findViewById(R.id.editTextChoice1);
+        final EditText choice2 = (EditText) findViewById(R.id.editTextChoice2);
+        final EditText choice3 = (EditText) findViewById(R.id.editTextChoice3);
+        final EditText choice4 = (EditText) findViewById(R.id.editTextChoice4);
+        final EditText choice5 = (EditText) findViewById(R.id.editTextChoice5);
+
         final ImageButton ButtonGoNext = (ImageButton) findViewById(R.id.imageButtonNext);
         ButtonGoNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(choices.this, shake.class);
+                intent.putExtra(EXTRA_CHOICE1, choice1.getText().toString());
+                intent.putExtra(EXTRA_CHOICE2, choice2.getText().toString());
+                intent.putExtra(EXTRA_CHOICE3, choice3.getText().toString());
+                intent.putExtra(EXTRA_CHOICE4, choice4.getText().toString());
+                intent.putExtra(EXTRA_CHOICE5, choice5.getText().toString());
+
+                // Check that the two first editText are fill
+                if (choice1.getText().toString().equals("") || choice2.getText().toString().equals("")) {
+                    Toast.makeText(choices.this,
+                            R.string.Error_nb_choices,
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 startActivity(intent);
             }
         });
--- Eze2chooz/app/src/main/java/toucoumer/eze2chooz/shake.java
+++ Eze2chooz/app/src/main/java/toucoumer/eze2chooz/shake.java
@@ -5,13 +5,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class shake extends AppCompatActivity {
+
+    final String EXTRA_CHOICE1 = "user_choice1";
+    final String EXTRA_CHOICE2 = "user_choice2";
+    final String EXTRA_CHOICE3 = "user_choice3";
+    final String EXTRA_CHOICE4 = "user_choice4";
+    final String EXTRA_CHOICE5 = "user_choice5";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shake);
+
+        Intent intent = getIntent();
+        TextView DisplayChoice1 = (TextView) findViewById(R.id.textViewChoice1);
+        TextView DisplayChoice2 = (TextView) findViewById(R.id.textViewChoice2);
+        TextView DisplayChoice3 = (TextView) findViewById(R.id.textViewChoice3);
+        TextView DisplayChoice4 = (TextView) findViewById(R.id.textViewChoice4);
+        TextView DisplayChoice5 = (TextView) findViewById(R.id.textViewChoice5);
+
+        if (intent != null) {
+            DisplayChoice1.setText(intent.getStringExtra(EXTRA_CHOICE1));
+            DisplayChoice2.setText(intent.getStringExtra(EXTRA_CHOICE2));
+            DisplayChoice3.setText(intent.getStringExtra(EXTRA_CHOICE3));
+            DisplayChoice4.setText(intent.getStringExtra(EXTRA_CHOICE4));
+            DisplayChoice5.setText(intent.getStringExtra(EXTRA_CHOICE5));
+        }
 
         final ImageButton ButtonGoToNext = (ImageButton) findViewById(R.id.imageButtonNext);
         ButtonGoToNext.setOnClickListener(new View.OnClickListener() {
